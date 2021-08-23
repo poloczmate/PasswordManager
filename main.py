b34ca038bb717b2a5c741db6d0e48388618e27c7
Mate Polocz <48856366+poloczmate@users.noreply.github.com>
2021-08-23 19:50:02 +0200

    first terminal gui

---
+++ main.py
@@ -0,0 +1,9 @@
+import os
+
+while True:
+    os.system("clear")
+    choose = input("Choose a option:\n1. Add password\n2. Get password\n3. Exit\nChoose:")
+    #Statements
+    print(choose)
+    if choose == "3":
+        break
---
+++ report.md
@@ -0,0 +1,6 @@
+# How password managers work
+* Store only hashed passswords --> Which hash function should i use?
+* Salt?
+* Master password --> XOR with all passwords
+
+# Hash algoritmus lehetosegek
